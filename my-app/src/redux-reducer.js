b66bbae1eac8c92b5f50c1cf1d81eed8a4d86a1c
Retamozo Matias <82097727+matiasreta@users.noreply.github.com>
2022-09-24 21:58:32 -0300

    store

--- my-app/src/redux-reducer.js
+++ my-app/src/redux-reducer.js
@@ -1,6 +1,31 @@
 import { DECREMENT,INCREMENT } from "./redux-actions";
 
 const initialState={
-    cuenta:0
+    contador:0,
+    contador_2:10
 }
 
+const reducer = (state= initialState, action)=>{
+
+    switch(action.type){
+        case DECREMENT:
+        {
+            return{
+                ...state, contador: state.contador+1,
+            }
+        }
+        case INCREMENT:
+        {
+            return{
+                ...state, contador: state.contador-1,
+            }
+        }
+        default:
+        {
+            return{...state}
+        }
+
+    }
+
+}
+export default reducer;
