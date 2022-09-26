f41a1c03aec9b102701ec49845fa1febaf1ce003
Retamozo Matias <82097727+matiasreta@users.noreply.github.com>
2022-09-26 01:55:56 -0300

    Cards

--- my-app/src/Card.jsx
+++ my-app/src/Card.jsx
@@ -1,32 +1,20 @@
 import React from "react";
-import { connect } from "react-redux";
-import {getCityData} from "./redux-actions";
 
 class Card extends React.Component{
-    
-    componentDidMount(){
-        console.log("se creó");
-        this.props.getCityData("Lima"); 
-
+    constructor(props){
+        super(props)
     }
     
-
+    
     render(){
         return(<>
     
             <h2>Card</h2>
             <div>
-               {this.props.city}
+                <p>Base?: {this.props.name} </p>
             </div>
             
             </>)}
 }
 
-const mapStateToProps=(state)=>{
-    return{city:state.city}
-}
-const mapDispatchToProps=(dispatch)=>{
-   return{getCityData:(city)=>{dispatch(getCityData(city))}}
-}
-
-export default connect(mapStateToProps,mapDispatchToProps)(Card);
+export default Card;
---
+++ my-app/src/SmartConteiner.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { connect } from "react-redux";
+import {getCityData} from "./redux-actions";
+import Card from "./Card";
+
+class SmartConteiner extends React.Component{
+    //state={cityes:[]}
+    componentDidMount(){
+        console.log("se creó");
+        this.props.getCityData("Lima"); 
+
+    }
+    
+    render(){
+        return(<>
+            <div>
+            <Card key={this.props.city.id} name={this.props.city.base}/>
+            </div>
+        </>)
+    }
+
+}
+
+
+const mapStateToProps=(state)=>{
+    return{city:state.city}
+}
+const mapDispatchToProps=(dispatch)=>{
+   return{getCityData:(city)=>{dispatch(getCityData(city))}}
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(SmartConteiner)
