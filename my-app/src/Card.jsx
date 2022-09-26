16b1e93accb7323ae424b68907f96dca03f8bddd
Retamozo Matias <82097727+matiasreta@users.noreply.github.com>
2022-09-26 00:53:51 -0300

    new action

--- my-app/src/Card.jsx
+++ my-app/src/Card.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { connect } from "react-redux";
+//import { connect } from "react-redux";
 //import {} from "./redux-actions";
 
 class Card extends React.Component{
--- my-app/src/redux-actions.js
+++ my-app/src/redux-actions.js
@@ -1,6 +1,7 @@
 export const INCREMENT = "INCREMENT"
 export const DECREMENT = "DECREMENT"
 export const GET_USERS = "GET_USERS"
+export const GET_CITY = "GET_CITY"
 
 export const decrement=()=>{
     return {type:DECREMENT}
@@ -14,4 +15,13 @@ export const getUsers =()=>{
         .then((response)=>response.json())
         .then((data)=>dispatch({type:GET_USERS,payload:data}))
     }
+}
+export const getCityData=(city)=>{
+    const key="4ae2636d8dfbdc3044bede63951a019b";
+    return function(dispatch){
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${userKey}`)
+        .then(response=>response.json())
+        .then(data=>dispatch({type:GET_CITY,payload:data}))
+    }
+
 }
