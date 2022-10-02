3893795dbced04ab00e77df4e42b939f0ff3d542
Retamozo Matias <82097727+matiasreta@users.noreply.github.com>
2022-10-02 20:45:18 -0300

    update

--- my-app/src/styles/StyleCard.js
+++ my-app/src/styles/StyleCard.js
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 
 export const StyleCard = styled.div`
+margin: auto;
+margin-top:20px;
 width: 180px;
 height: 200px;
 border-radius:8px;
-background-color: #e4f7f6;
+background-color: hsl(177, 54%, 93%);
 border: 1px solid black;
 `
--- my-app/src/styles/StyleConteiner.js
+++ my-app/src/styles/StyleConteiner.js
@@ -2,8 +2,9 @@ import styled from "styled-components";
 
 export const StyledConteiner= styled.div`
 background-color: #e1adff;
-display: flex;
-width: 1000px;
+display: grid;
+grid-template-columns: 1fr 1fr 1fr 1fr;
+max-width: 800px;
 margin: auto;
 `
 
