b3e73938001701f0c913a993bcd6786d00fdfc39
Retamozo Matias <82097727+matiasreta@users.noreply.github.com>
2022-10-03 17:40:16 -0300

    Update StyleCard.js

--- my-app/src/styles/StyleCard.js
+++ my-app/src/styles/StyleCard.js
@@ -14,13 +14,16 @@ export const StyleTwoOptions = styled.div`
  display: grid;
  grid-template-columns: 1fr 1fr;
 
- button.discard{
-    border-radius: 0px 0px 7px 0px;
-    border:1px solid black;
+button.discard{
+   border-radius: 0px 0px 7px 0px;
+   border:2px solid #63d6df;
+   border-left:2px solid #63d6df;
+
  }
- button.like{
-    border-radius: 0px 0px 0px 7px;
-    border:1px solid black;
+button.like{
+   border-radius: 0px 0px 0px 7px;
+   border:2px solid #63d6df;
+   border-right:0px solid #63d6df;
  }
 
 `
